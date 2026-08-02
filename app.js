1f3caac0627aef508e02ff82883fc54bbb3cd774
marwanthedev <tmarawan11@gmail.com>
2026-08-02 14:29:49 +0000

    update port to 3001 for staging

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http')
 http.createServer(function(req,res){
 	res.write('We sgo2 we go we go: Env is staging');
 	res.end();
-}).listen(3000);
+}).listen(3001`);
 
 
 console.log('Server stareted on port:3000');
