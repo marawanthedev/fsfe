f7b63b8c976d0c185223b7cd0c23ebd73d74ddd3
marwanthedev <tmarawan11@gmail.com>
2026-08-02 14:10:41 +0000

    update message

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http')
 http.createServer(function(req,res){
-	res.write('We sgo2 we go we go');
+	res.write('We sgo2 we go we go: Env is staging');
 	res.end();
 }).listen(3000);
 
