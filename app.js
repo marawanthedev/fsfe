d636948fcf82ad8a62eaedfbb217cc99156215cc
marwanthedev <tmarawan11@gmail.com>
2026-08-03 12:24:40 +0000

    update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const PORT = process.env.PORT || 3000;
+
 const http = require('http')
 http.createServer(function(req,res){
-	res.write('We sgo2 we go we go: Env is staging');
+	res.write('we go: Env is staging');
 	res.end();
-}).listen(3001`);
+}).listen(3001);
 
 
 console.log('Server stareted on port:3000');
